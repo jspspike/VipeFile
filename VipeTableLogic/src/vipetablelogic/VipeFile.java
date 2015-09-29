f349d01e44929f58f9a369ff3f9185a7f1c97dcc
unknown <johnson_849323@VG162007A8156D.leanderisd.org>
2015-09-29 14:05:06 -0500

    Added base files

---
+++ VipeTableLogic/src/vipetablelogic/VipeFile.java
@@ -0,0 +1,92 @@
+package vipetablelogic;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author johnson_849323
+ */
+public class VipeFile {
+    /*A data class that keeps track of chunks as they are allowcated across the directory
+    then displayed in the directory. Will contain file id, name, color, size and chunks
+    */
+
+   ArrayList<Chunk> chunks;
+   int fileSize;
+   String fileName;
+   Color sectorColor;
+   static int fileID;
+
+    void VipeFile(int size, String name, Color color, int fileID) {
+        setFileSize(size);
+        setFileName(name);
+        setSectorColor(color);
+        this.fileID = fileID;
+        
+        chunks = new ArrayList<Chunk>();
+        
+        // Serialize fileID
+        
+        
+    }
+   
+    public ArrayList<Chunk> getChunks() {
+        return chunks;
+    }
+
+    public void setChunks(ArrayList<Chunk> chunks) {
+        this.chunks = chunks;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Color getSectorColor() {
+        return sectorColor;
+    }
+
+    public void setSectorColor(Color sectorColor) {
+        this.sectorColor = sectorColor;
+    }
+
+    public int getFileID() {
+        return fileID;
+    }
+
+
+   @Override
+   public String toString() {
+       /* Dispays file id, name, color, size and chunks
+       <fileID> <fileName> (<sectorColor>, <fileSize>s)
+          <chunks...>
+       Example:
+        1 Quiz.doc (Red, 15s)
+        [0-6] [12-18] [39-40]
+       */
+       String cstrings = "";
+       for (int i = 0; i < chunks.size(); i++) {
+           cstrings += "<" + chunks.get(i) + "> ";
+       }
+       return "" + fileID + " " + fileName + " (" + sectorColor + ", " + fileSize + "s)\n    " + cstrings;
+   }
+}
---
+++ VipeTableLogic/src/vipetablelogic/VipeTableLogic.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vipetablelogic;
+
+/**
+ *
+ * @author johnson_849323
+ */
+public class VipeTableLogic {
+    
+    private Directory dir;
+    
+    public VipeTableLogic() {
+        
+    }
+    
+    private String getChunkString(int fileID) {
+        return "";
+    }
+    
+    private int getAvailableSize() {
+        return 0;
+    }
+    
+    private Chunk getNextChunk() {
+        return new Chunk(0,0);
+    }
+    
+    private void writeFile(VipeFile file) {
+        
+    }
+    
+    public void writeGrid() {
+        
+    }
+    
+    private void deleteFile(int fileID) {
+        
+    }
+    
+    private void editFile(int fileID, int sectorChange) {
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
